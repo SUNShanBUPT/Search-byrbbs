2f034c2ea97f59bb654423e6ffa42df15d57eb81
SUNShanBUPT <429749446@qq.com>
2019-04-28 19:56:45 +0800

    version 3.2

--- ByrBBS.py
+++ ByrBBS.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import re
+import time
 
 
 class ByrBbs(object):
@@ -14,6 +15,7 @@ class ByrBbs(object):
         self.total_pages = 10
         self.keys = []
         self.section = ''
+        self.date = '2016-01-01'
 
     def login(self):
         """Login to byr_bbs"""
@@ -25,7 +27,7 @@ class ByrBbs(object):
         """Search the given section in ByrBbs"""
         for i in range(self.total_pages):
             section_page = self.session.get("https://bbs.byr.cn/board/" + self.section + '?p=' + str(i), headers=self.header)
-            print('Searching page ' + str(i + 1))
+            print('Searching section-' + self.section + ' page ' + str(i + 1))
 
             res = r'<td\sclass="title_9"><a\shref="(.*?)">(.*?)</a>'  # to get posts information in each section pages
             reg = re.compile(res)
@@ -33,66 +35,105 @@ class ByrBbs(object):
 
             for forum_post in forum_posts:
                 post_url = "http://bbs.byr.cn" + forum_post[0]
-                print('Searching ' + post_url)
+                # print('Searching ' + post_url)
                 post_info = self.session.get(post_url, headers=self.header).text
 
                 regname = r'<span\sclass="a-u-name"><a\shref=".*?">(.*?)</a>.*?<div\sclass="a-content-wrap">(.*?)<font\sclass="f000"></font>'
                 resname = re.compile(regname)
                 post_content = re.findall(resname, post_info)
 
-                self.search_keys(forum_post, post_content)
+                text = self.get_text(forum_post, post_content)
+                if self.search_keys(forum_post, text):
+                    self.download(forum_post, text)
 
-    def search_keys(self, forum_post, post_content):
+    def get_text(self, forum_post, post_content):
         try:
             tempsrc = post_content[0][1].replace('<br />', '\n').replace(' ', '  ')
             # remove picture
-            resformat = r'<a.*?>.*?</a>'
-            tempstr2 = re.sub(resformat, '', tempsrc)
+            tempstr = re.sub(r'<a.*?>.*?</a>', '', tempsrc)
             # remove font
-            resformat2 = r'<font.*?>|</font>'
-            tempstr3 = re.sub(resformat2, '', tempstr2)
+            tempstr = re.sub(r'<font.*?>|</font>', '', tempstr)
             # remove img
-            resformat3 = r'<img.*?/>'
-            tempstr4 = re.sub(resformat3, '', tempstr3)
-            sepe = '*' * 40
-            tempstr = sepe + '\n' + tempstr4 + '\n'
+            tempstr = re.sub(r'<img.*?/>', '', tempstr)
+            sepe = '*' * 20
+            tempstr = sepe + forum_post[1] + '\n' + tempstr + '\n'
 
+            return tempstr
+        except IndexError:
+            return None
+
+    def get_text_time(self, text):
+        regtime = r'北邮人论坛 [(](.*?)[)]'
+        restime = re.compile(regtime)
+        time_string = re.findall(restime, text)[0]
+
+        temp_time = time_string[-4:] + '-' + time_string[4:7] + '-' + time_string[8:10]
+
+        temp_time = re.sub(r'Jan', '01', temp_time)
+        temp_time = re.sub(r'Feb', '02', temp_time)
+        temp_time = re.sub(r'Mar', '03', temp_time)
+        temp_time = re.sub(r'Apr', '04', temp_time)
+        temp_time = re.sub(r'May', '05', temp_time)
+        temp_time = re.sub(r'Jun', '06', temp_time)
+        temp_time = re.sub(r'Jul', '07', temp_time)
+        temp_time = re.sub(r'Aug', '08', temp_time)
+        temp_time = re.sub(r'Sep', '09', temp_time)
+        temp_time = re.sub(r'Oct', '10', temp_time)
+        temp_time = re.sub(r'Nov', '11', temp_time)
+        temp_time = re.sub(r'Dec', '12', temp_time)
+
+        return temp_time
+
+    def search_keys(self, forum_post, text):
+        if not(text is None):
             counter = 0
             for key in self.keys:
-                if tempstr.find(key) != -1:
+                if text.find(key) != -1:
                     counter = counter + 1
 
-            if counter == len(self.keys):
+            if counter == len(self.keys) and self.get_text_time(text) > self.date:
                 print("http://bbs.byr.cn" + forum_post[0])
                 print(forum_post[1])
-                print(tempstr)
+                return True
+            else:
+                return False
+        else:
+            return False
 
-                filename = forum_post[1] + '.txt'
-                if os.path.exists(filename):
-                    pass
-                else:
-                    txt = open(filename, 'w+')
-                    txt.write("http://bbs.byr.cn" + forum_post[0] + '\n')
-                    txt.write(forum_post[1] + '\n')
-                    txt.write(tempstr)
-                    txt.close()
-        except IndexError:
+    def download(self, forum_post, text):
+        """filename = time.strftime("%Y-%m-%d-", time.localtime()) + self.section + re.sub(r"[\/\\\:\*\?\"\<\>\|]", '_',
+                                                                                        forum_post[1]) + '.txt'
+        """
+        filename = self.get_text_time(text) + '_'+ self.section + re.sub(r"[\/\\\:\*\?\"\<\>\|]", '_', forum_post[1]) + '.txt'
+
+        if os.path.exists(filename):
             pass
+        else:
+            txt = open(filename, 'w+')
+            txt.write("http://bbs.byr.cn" + forum_post[0] + '\n')
+            txt.write(forum_post[1] + '\n')
+            txt.write(text)
+            txt.close()
 
-    def start(self, section, total_pages, keys):
+    def start(self, section, total_pages, keys, date):
         self.login()
         self.total_pages = total_pages
         self.keys = keys
         self.section = section
+        if date:
+            self.date = date
         self.search_section()
 
 
 def main():
-    keys = ['网研', '保研']  # Search keys
+
+    keys = ['硕士', '内推']  # Search keys
     b = ByrBbs('', '')  # Your id and password
-    b.start('AimGraduate', 8, keys)  # 'The section you want to search', 'Total pages'
+    for section in ['IT', 'jobinfo']:
+        b.start(section, 10, keys, '2019-01-01')  # 'The section you want to search', 'Total pages', 'keys', 'search posts after this date'
 
 
 if __name__ == "__main__":
     main()
+
 
